7bd9e2ff1dd62d28a680a76e8845edc5e124ac03
Advent1013 <Vishahan@users.noreply.github.com>
2017-05-14 12:43:35 -0400

    Add files via upload

---
+++ Iteration 2/Client.java
@@ -0,0 +1,175 @@
+/**
+ * Client for a TFTP
+ *
+ *Iteration 1:
+ * Client asks user to make R/W request.
+ * Request sent to server at port 23 and receives response.
+ * Performs file operation.
+ * Little to no error checking done for now.
+ *      
+ */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client{
+	private DatagramSocket 	soc;
+	private helplib 		help;
+	private String 			workingDir;
+	private InetAddress		serverAddress;
+	private int				Port;
+	
+	public Client(int port, InetAddress addr, String dir, boolean verbose){
+		help = new helplib("Client", verbose);
+		try{ soc = new DatagramSocket(); } 
+		catch(SocketException se){ help.print("Failed to create Socket."); System.exit(1); }
+		workingDir	= dir;
+		Port 		= port;
+		serverAddress 	= addr;
+		help.print("Initialized");
+	}
+	
+	public void begin(){
+		int request;
+		//Get the request from the user.
+		Scanner sc2 = new Scanner(System.in);
+		while(true){
+			help.print("What type of request would you like to make(RRQ/WRQ)?");
+			
+			String input = sc2.nextLine();
+			if(input.toUpperCase().equals("RRQ")){ request=1; break;}
+			if(input.toUpperCase().equals("WRQ")){ request=2; break;}
+			help.print("Invalid Request! Select either 'RRQ'(Read) or 'WRQ' (Write)");
+		}
+		
+		if(request==1){
+			//Read Request;
+			
+			//Get the file to read from the server.
+			help.print("Please enter the name of the file you wish to access:");
+			String sendFile = sc2.nextLine();
+			
+			//Get the file to save data to.
+			FileOutputStream FOut;
+			while(true){
+				help.print("Please enter the name of the file you want to save the data to:");
+				String saveFile = sc2.nextLine();
+				FOut = help.OpenOFile(workingDir + saveFile, true);
+				if(FOut==null) help.print("File already exists! Please use another!");
+				else		   break;
+			}
+			
+			int numBlock = 0; //Total number of blocks;
+			int curBlock = -1; //The current block the receive is on;
+			
+			//Attempt to handshake with the server to get the size of the file from the server;
+			//Send the request.
+			System.out.println();
+			help.print("Attempting request...");
+			
+			Packet req = new Packet(1, sendFile, "netascii");
+			Packet rec = help.sendReceive(req, soc, serverAddress, Port);
+			serverAddress = rec.GetAddress();
+			Port = rec.GetPort();
+			
+			if(!help.isOkay(rec, 4)){ return; }
+			numBlock=rec.GetPacketN();
+			
+			System.out.println();
+			help.print("Request Success, receiving "+numBlock+" blocks.");
+			try{
+				//Once Handshake is established, initiate file transfer.
+				help.print("Initiating file transfer.");
+				Packet ack = new Packet(numBlock);
+				//Loop that transfers the file.
+				while(curBlock < numBlock){
+					rec = help.sendReceive(ack, soc, serverAddress, Port);
+					if(!help.isOkay(rec, 3)){ return; }
+					//Makes sure the packet is valid and then writes it to file.
+					if(curBlock+1==rec.GetPacketN()){ 
+						curBlock++; 
+						help.WriteData(FOut, rec.GetData());
+					}
+					else{
+						help.print("Invalid Packet Recieved! Closing.");
+						System.exit(1);
+					}
+					//Create response with the current block received;
+					ack = new Packet(rec.GetPacketN());
+				}
+				ack = new Packet(numBlock);
+				ack = help.sendReceive(ack, soc, serverAddress, Port);
+			}
+			catch (Exception e) { e.printStackTrace(); System.exit(1); }
+			System.out.println();
+			help.print("File transfer complete!");
+			try { FOut.close(); } catch (IOException e) { e.printStackTrace(); }
+		}
+		else{
+			//Write Request;
+			help.print("Please enter the save location on the server:");
+			String saveFile = sc2.nextLine();
+			String sendFile;
+			//Get the file to save data to.
+			FileInputStream FIn;
+			while(true){
+				help.print("Please enter the name of the file you wish to read from:");
+				sendFile = sc2.nextLine();
+				FIn = help.OpenIFile(workingDir + sendFile);
+				if(FIn==null) 	help.print("File doesnt exist! Please use another!");
+				else		   	break;
+			}
+			help.print("Attempting request...");
+			//Getting the size of the file;
+			int numBlock = 0;
+			int curBlock = 0;
+			try { numBlock = (int)(FIn.getChannel().size()/Packet.DATASIZE);
+			} catch (IOException e) { e.printStackTrace(); }
+			
+			//Send the request;
+			Packet req = new Packet(2, saveFile, "netascii");
+			Packet rec = help.sendReceive(req, soc, serverAddress, Port);
+			serverAddress = rec.GetAddress();
+			Port = rec.GetPort();
+			if(!help.isOkay(rec, 4)){ return; }
+
+			help.print("Request Success, sending "+numBlock+" blocks.");
+			//Ready for file transfer;
+			req = new Packet(numBlock);
+			rec = help.sendReceive(req, soc, serverAddress, Port);
+			if(!help.isOkay(rec, 4)){ return; }
+			
+			while(curBlock <= numBlock){
+				byte[] bData = help.ReadData(FIn, curBlock, Packet.DATASIZE);
+				Packet ack = new Packet(curBlock,bData);
+				rec = help.sendReceive(ack, soc, serverAddress, Port);
+				if(!help.isOkay(rec, 4)){ return; }
+				curBlock++;
+			}
+			
+			help.print("File transfer complete!");
+		}
+		sc2.close();
+	}
+	
+	public static void main(String[] args){
+		try {
+			boolean verbose;
+			Scanner sc = new Scanner(System.in);
+			while(true){
+				System.out.println("Would you like to run it in verbose mode (Y/N)?");
+				
+				String input = sc.nextLine();
+				if(input.toUpperCase().equals("Y")){ verbose=true; break;}
+				if(input.toUpperCase().equals("N")){ verbose=false; break;}
+				System.out.println("Invalid Mode! Select either 'Y'(Yes), 'N'(No)");
+			}
+			Client c = new Client(69, InetAddress.getLocalHost(),new java.io.File( "." ).getCanonicalPath() + "\\",verbose);
+			c.begin();
+			sc.close();
+		} catch (Exception e) { e.printStackTrace(); }
+		
+	}
+}
---
+++ Iteration 2/Server.java
@@ -0,0 +1,298 @@
+/**
+ * Client for a TFTP
+ *
+ *Iteration 1:
+ * Server class starts a ServerMaster thread and waits for user
+ *   to press OK, on a displayed popup, to end ServerMaster.
+ * ServerMaster continually awaits a connection at port 69.
+ * ServerMaster receives connection and passes it to a
+ *   WorkerHandler thats working on any request with the Client's
+ *   port and address, or creates a WorkerHandler if none exist.
+ * Newly created WorkerHandler starts up a ServerWorker thread,
+ *   works on handling the client's request.
+ * Upon ServerMaster ending, waits for all workers to complete,
+ *   before closing and refuses any further connections.
+ */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import javax.swing.*;
+
+//When program is run, server is called to create a Server Master;
+public class Server{
+	public static void main(String[] args){
+		boolean verbose;
+  	  Scanner sc = new Scanner(System.in);
+  	  while(true){
+				System.out.println("Would you like to run it in verbose mode (Y/N)?");
+				
+				String input = sc.nextLine();
+				if(input.toUpperCase().equals("Y")){ verbose=true; break;}
+				if(input.toUpperCase().equals("N")){ verbose=false; break;}
+				System.out.println("Invalid Mode! Select either 'Y'(Yes), 'N'(No)");
+		 }
+  	 sc.close();
+		ServerMaster SM = new ServerMaster(verbose);
+		SM.start();
+		JOptionPane.showMessageDialog(null, "Press 'OK' at any point to quit");
+		SM.Stop();
+	}
+}
+
+//ServerMaster, awaits for connection and passes on to WorkerHandler;
+class ServerMaster extends Thread{
+	private DatagramPacket  		rpkt;
+	private DatagramSocket 			soc;
+	private helplib 				help;
+	private boolean					verbose;
+	private boolean					running;
+	private ArrayList<WorkerHandler> workers;
+	
+	public ServerMaster(boolean Verbose){
+		//Creates a DatagramSocket
+		help = new helplib("Server", Verbose);
+		workers = new ArrayList<WorkerHandler>();
+		try{ soc = new DatagramSocket(69); } 
+		catch(SocketException se){ help.print("Failed to create socket!"); System.exit(1); }
+		help.print("Initialized");
+		running = true;
+		verbose = Verbose;
+	}
+	//Tells the ServerMaster to quit;
+	public void Stop(){
+		running = false;
+		help.print("Closing server, waiting for workers to complete");
+	}
+	//Main ServerMaster logic;
+	public void run(){
+		//Server will listen and timeout and loop back until it gets a request;
+		while(running || !allDone()){
+			byte[] rec = new byte[Packet.PACKETSIZE];
+			rpkt = new DatagramPacket(rec,rec.length);
+			try { 
+				soc.setSoTimeout(500);
+				soc.receive(rpkt);
+				//If server times out, the following part is skipped;
+				help.print("Got a connection, deligating to worker.");
+				help.printd("The bytes recieved are:\n"+ Arrays.toString(rec));
+				handlePacket(new Packet(rec),rpkt.getPort(),rpkt.getAddress());
+			} catch (IOException e) 
+			{ 
+				soc.close();
+				try{ soc = new DatagramSocket(69); } 
+				catch(SocketException se){ help.print("Failed to create socket!"); System.exit(1); }
+			}
+			
+		}
+		soc.close();
+		help.print("All workers have completed, exiting.");
+		System.exit(1);
+	}
+	
+	private boolean allDone(){
+		for(int i=0;i<workers.size();i++)
+			if(!workers.get(i).isDone()) return false;
+		return true;
+	}
+	
+	//Sends the packet to a worker thats active and has the same address, else create a new one;
+	private void handlePacket(Packet request, int port, InetAddress address){
+		//Locates appropriate Worker to send packet to;
+		for(int i=0;i<workers.size();i++){
+			if(!workers.get(i).isDone()){
+				if(workers.get(i).address.equals(address) && workers.get(i).port == port){
+					workers.get(i).passReq(request);
+					return;
+				}
+			}
+		}
+		//If the program is still running, accepts a new request;
+		if(running)
+			workers.add(new WorkerHandler(port,address,request,verbose));
+		
+	}
+}
+
+//This class wraps around the ServerWorker class so that MetaData can be retrieved
+class WorkerHandler{
+	public int port;
+	public InetAddress address;
+	private ServerWorker worker;
+	private BlockingQueue<Packet> bQueue;
+	
+	//Constructor for worker;
+	public WorkerHandler(int Port, InetAddress clientAddress, Packet request, boolean verbose){
+		bQueue 	= new ArrayBlockingQueue<Packet>(10);
+		port	= Port;
+		address = clientAddress;
+		worker 	= new ServerWorker(Port,clientAddress,request,verbose,bQueue);
+		worker.start();
+	}
+	
+	//Wait for ServerWorker Thread to complete;
+	public void Wait(){
+		try { worker.join(); }
+		catch (InterruptedException e) { e.printStackTrace(); }
+	}
+	
+	//Checks if ServerWorker Thread to complete;
+	public boolean isDone(){
+		if(worker.getState()==Thread.State.TERMINATED) return true;
+		return false;
+	}
+	
+	//Passes the request to the thread;
+	public void passReq(Packet request){
+		try { bQueue.put(request);
+		} catch (InterruptedException e) { e.printStackTrace(); }
+	}
+}
+
+//Worker thread that handles a client request;
+class ServerWorker extends Thread{
+	int port;
+	InetAddress address;
+	Packet mainReq;
+	DatagramSocket soc;
+	helplib help;
+	private BlockingQueue<Packet> bQueue;
+	
+	//Constructor;
+	public ServerWorker(int Port, InetAddress clientAddress, Packet request, boolean verbose, BlockingQueue<Packet> Queue){
+		port 	= Port;
+		address = clientAddress;
+		mainReq = request;
+		bQueue 	= Queue;
+		help	= new helplib("ServerWorker@"+Port, verbose);
+		help.print("Worker created to handle the request:\n"+request);
+		help.print("From:\nPort:    "+port+"\nAddress: "+ address);
+		try{ soc = new DatagramSocket(); } 
+		catch(SocketException se){ help.print("Failed to create Socket."); System.exit(1); }
+	}
+	
+	//Main ServerWorker logic;
+	public void run(){
+		if(mainReq.GetRequest()==1){
+			//Read request;
+			FileInputStream FIn = help.OpenIFile(mainReq.GetFile());
+			if(FIn==null){ System.exit(1); }
+			int numBlock = 0;
+			int curBlock = 0;
+			try { numBlock = (int)(FIn.getChannel().size()/Packet.DATASIZE);
+			} catch (IOException e) { e.printStackTrace(); }
+			help.print("File located, Initiating transfer of "+ numBlock + " blocks.");
+			Packet ack = new Packet(numBlock);
+			help.sendPacket(ack, soc, address, port);
+			Packet rec = help.recievePacket(soc);
+			if(!help.isOkay(rec, 4)){ 
+				Packet ERR = new Packet(4,"Invalid packet received.");
+				help.sendPacket(ERR, soc, address, port);
+				return; 
+			}
+			if(checkAddress(rec)){
+				Packet ERR = new Packet(5,"Packet received from unknown sender.");
+				help.sendPacket(ERR, soc, address, port);
+				return; 
+			}
+			if(rec.GetRequest()!=4) System.exit(1);
+			//File transfer loop;
+			while(curBlock <= numBlock){
+				byte[] bData = help.ReadData(FIn, curBlock, Packet.DATASIZE);
+				ack = new Packet(curBlock,bData);
+				help.sendPacket(ack, soc, address, port);
+				rec = help.recievePacket(soc);
+				if(!help.isOkay(rec, 4)){ 
+					Packet ERR = new Packet(4,"Invalid packet received.");
+					help.sendPacket(ERR, soc, address, port);
+					return; 
+				}
+				if(checkAddress(rec)){
+					Packet ERR = new Packet(5,"Packet received from unknown sender.");
+					help.sendPacket(ERR, soc, address, port);
+					return; 
+				}
+				
+				if(rec.GetRequest()==4){
+					curBlock++;
+				} else System.exit(1);
+			}
+			ack = new Packet(curBlock);
+			help.sendPacket(ack, soc, address, port);
+			try { FIn.close(); } catch (IOException e) { e.printStackTrace(); }
+			help.print("File transfer complete!");
+		}
+		else{
+			//Write Request
+			FileOutputStream FOut = help.OpenOFile(mainReq.GetFile(), true);
+			Packet ack = new Packet(0);
+			help.sendPacket(ack, soc, address, port);
+			Packet rec = help.recievePacket(soc);
+			if(!help.isOkay(rec, 4)){ 
+				Packet ERR = new Packet(4,"Invalid packet received.");
+				help.sendPacket(ERR, soc, address, port);
+				return; 
+			}
+			if(checkAddress(rec)){
+				Packet ERR = new Packet(5,"Packet received from unknown sender.");
+				help.sendPacket(ERR, soc, address, port);
+				return; 
+			}
+			
+			int numBlock = rec.GetPacketN();
+			int curBlock = -1;
+			ack = new Packet(0);
+			help.sendPacket(ack, soc, address, port);
+			while(curBlock < numBlock){
+				rec = help.recievePacket(soc);
+				if(!help.isOkay(rec, 3)){ 
+					Packet ERR = new Packet(4,"Invalid packet received.");
+					help.sendPacket(ERR, soc, address, port);
+					return; 
+				}
+				if(checkAddress(rec)){
+					Packet ERR = new Packet(5,"Packet received from unknown sender.");
+					help.sendPacket(ERR, soc, address, port);
+					return; 
+				}
+				
+				//Makes sure the packet is valid and then writes it to file.
+				if(curBlock+1==rec.GetPacketN()){ 
+					curBlock++; 
+					help.WriteData(FOut, rec.GetData());
+				}
+				else{
+					help.print("Invalid Packet Recieved! Closing.");
+					System.exit(1);
+				}
+				//Create response with the current block received;
+				ack = new Packet(rec.GetPacketN());
+				help.sendPacket(ack, soc, address, port);
+			}
+			try { FOut.close(); } catch (IOException e) { e.printStackTrace(); }
+			help.print("File transfer complete!");
+		}
+		
+	}
+	
+	/*//Gets a packet from the parent WorkerHandler.
+	private Packet receivePacket(){
+		try {
+			Packet p = bQueue.take();
+			help.printd("Got the following packet:\n"+p);
+			return p;
+		} catch (InterruptedException e) { e.printStackTrace(); }
+		return null;
+	}*/
+	
+	private boolean checkAddress(Packet P){
+		if(P.GetPort()==port && P.GetAddress()==address) return true;
+		return false;
+	}
+	
+}
