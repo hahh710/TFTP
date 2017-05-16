e9b5d5657eef0998e5199c70a8a918aa3b54c655
ramidaham <ramidaham@cmail.carleton.ca>
2017-05-16 18:58:02 -0400

    submitted version

---
+++ iteration2-Final(s)/README.txt
@@ -0,0 +1,130 @@
+Read Me
+
+Name: 
+Adebola Shittu         100918348 - Error Simulator 
+Rami Daham             100995251 - Error Simulator\Reports and Documentation
+Vishahan Thilagakumar  100994856 - Server (implement error handling - updated file transfer)
+Kariharan Thilagakumar 100922048 - Client (implement error handling - updated file transfer) 
+Huno Ha:               100868604 - Debugging
+
+Documentation Files Included:
+ -README - "README.md"
+ -UML    - "UML-iteration2.png"
+ -UCM    - "FileTransfer.pdf"
+ 
+ Test Files for file transfer are:
+ -"test.txt"
+ -"test2.txt"
+ 
+=====================================================================
+File Transfer Protocol
+Sending File format from the Client to the intermediate and finally 
+to the Server which in turns send back an acknowledgement
+
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+SETTING UP THE CLIENT , ERROR SIMULATOR AND SERVER
+a. Create a folder name "client" in the program directory to store the client files
+b. Create a folder name "server" in the program directory to store the server files
+c. Make sure there is something in the file being copied for a RRQ request
+d. Select Error to implement from the simulator
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Client:
+  * Ensure that all files are on the same machine
+    If not, change the Inet Address to match the IP address of the intermediate
+  * The client support for receiving packet
+  * Recognise opcode changes
+
+
+===================================================================
+Getting IP Address
+
+* Run command Prompt on the machine
+* On window Machine (Enter Ip config) or ipconfig on Linux Machine
+* if the machine are using wifi
+      * Find Ipv4 (thats the IP address)
+      * change the InetAddress to this the new Address
+* If machine is using ethernet
+      * Find eth0 (thats the IP Address)
+      * Change the InetAddress to this new Address
+===================================================================
+
+Intermediate:
+  * Ensure the client and intermediate are on the same machine and 
+    if not, change the IP Address as described above
+  * Ensure the receiving Port is the same as the client port
+  * Ensure the Sending port is the same as the server receiveing port
+  * Run the intermediate first before running the client
+
+
+Server:
+  * Ensure the server and intermediate are on the same machine and 
+    if not, change the IP Address as described above
+  * Ensure the receiving Port is the same as the sending port of intermediate
+  * Ensure the Sending port is the same as the server receiveing port
+  * Run the intermediate first then the server before running the client
+
+=========================================================================
+Running the Program
+Step 1: Start the Sever 
+Step 2: Start the Error Simulator
+Step 3: Start the Client
+
+The client prompts the user for whether it is a RRQ or WRQ then 
+asks the client for the mode of Operation(Verbose then for using the ErrorSimulator).
+The client initiate the communication with the server through handshaking and then 
+commerce connection with the Server.
+
+Once the file transfer is done, the ErrorSimulator and the client will ask the user for
+further operations.
+
+The Error Simulator: acts as a middle man between the client and the server.
+   For RRQ: Pass on the packet from the client to the server and then waits for acknowledgement 
+   from the server and then pass the acknowledgement back to the client.
+   
+
+
+================================================================================
+Helper Function 
+* Helplib
+* Packet
+
+Instruction:
+* Ensure that helplib and packet are in the file project as the client, simulator and server
+
+
+Helplib
+An assortment of reusable functins accorss the classes. Helplib has a method for:
+* Sending Packet
+* receiving Packet
+* Print with Verbose communication
+* handing File input and output
+
+Packet
+This hides away manually dealing with creating byte to be sent in Datagram Packet.
+
+Anything other than Above will give an Invalid Packet and the 
+Server will throw an exception
+
+Packet Format follows TFTP format specifications 
+   Type   Op #     Format without header
+   
+          2 bytes    string   1 byte     string   1 byte
+          -----------------------------------------------
+   RRQ/  | 01/02 |  Filename  |   0  |    Mode    |   0  |
+   WRQ    -----------------------------------------------
+          2 bytes    2 bytes       n bytes
+          ---------------------------------
+   DATA  | 03    |   Block #  |    Data    |
+          ---------------------------------
+          2 bytes    2 bytes
+          -------------------
+   ACK   | 04    |   Block #  |
+          --------------------
+          2 bytes  2 bytes        string    1 byte
+          ----------------------------------------
+   ERROR | 05    |  ErrorCode |   ErrMsg   |   0  |
+          ----------------------------------------
+   (https://tools.ietf.org/html/rfc1350)
+  
---
+++ iteration2-Final(s)/src/helplib.java
@@ -0,0 +1,229 @@
+
+/**
+ * A collection of functions that is to be used across
+ *   the various classes so as to avoid duplicate functions.
+ *
+ *Iteration Exclusive:
+ * Future Iterations may add more helper functions when needed.
+ *      
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.Arrays;
+
+//A collection of helper libraries to automate certain events.
+public class helplib {
+	private String name;
+	private boolean verbose;
+	public helplib(String Name, boolean Verbose){
+		name    = Name;
+		verbose = Verbose;
+	}
+	//Open File and make sure it exists.
+	public FileInputStream OpenIFile(String path){
+		try { 
+			FileInputStream in = new FileInputStream(path); return in;
+		} catch (FileNotFoundException e) {
+			print("FileIO::ERROR::File not found. "+ path); return null; 
+		}
+	}
+	public FileOutputStream OpenOFile(String path, boolean notExist){
+		File dir = new File(path);
+		if(!dir.exists() && notExist){
+			try { dir.createNewFile(); } 
+			catch (IOException e) {  }
+		}
+		else if(dir.exists() && notExist){ return null; }
+		
+		try { 
+			FileOutputStream out = new FileOutputStream(dir,true); return out;
+		} catch (FileNotFoundException e) {
+			//The code should just quit if it somehow comes to this.
+			e.printStackTrace(); System.exit(1);
+		}
+		return null;
+	}
+	
+	//Read bytes from a File at position.
+	public byte[] ReadData(FileInputStream fIn, int block, int size){
+		try { 
+			long fileSize = fIn.getChannel().size();
+			byte[] out = new byte[size];
+			fIn.read(out);
+			return out;
+		} catch (IOException e) { e.printStackTrace(); }
+		return null;
+	}
+	
+	//Write bytes to end of file.
+	public void WriteData(FileOutputStream fOut, byte[] block){
+		int i = 0;
+		byte[] write = null;
+		for(i = 0; i<block.length; i++){
+			if(block[i]==0){ 
+				write = new byte[i];
+				for(int j = 0; j<i; j++){
+					write[j] = block[j];
+				}
+				break;
+			}
+		}
+		if(i>=block.length) write = block;
+		try { fOut.write(write);
+		} catch (IOException e) {e.printStackTrace(); }
+	}
+	
+	//Custom Print statement that adds the class name with it;
+	public void print(String Message){
+		System.out.println(name+"::: "+Message);
+	}
+	
+	//Custom Print that doesn't get printed unless stated otherwise;
+	public void printd(String Message){
+		if(verbose){
+			System.out.println(name+":D: "+Message);
+		}
+		
+	}
+	
+	//Helper shortens the send command;
+	public void sendPacket(Packet p, DatagramSocket soc, InetAddress addr, int port){
+		try{
+			byte[] bReq = p.toBytes();
+			DatagramPacket spkt = new DatagramPacket(bReq, bReq.length, addr, port);
+			soc.send(spkt);
+			printd("\nSent the packet: "+p+"\nWith the following bytes:\n"+byteToString(bReq)+"\n");
+		}
+		catch (Exception e) { e.printStackTrace(); System.exit(1); }
+	}
+	
+	/*//Helper shortens the receive command;
+	public byte[] recievePacket(DatagramSocket soc){
+		try{
+			byte[] bRec = new byte[Packet.PACKETSIZE];
+			DatagramPacket rpkt = new DatagramPacket(bRec,bRec.length);
+			soc.receive(rpkt);
+			printd("Got the following bytes:\n"+byteToString(bRec));
+			return bRec;
+		}
+		catch (Exception e) { e.printStackTrace(); System.exit(1); }
+		return null;
+	}*/
+	
+	//Helper shortens the receive command;
+	public Packet recievePacket(DatagramSocket soc){
+		Packet rec = new Packet();
+		try{
+			byte[] bRec = new byte[Packet.PACKETSIZE];
+			DatagramPacket rpkt = new DatagramPacket(bRec,bRec.length);
+			soc.receive(rpkt);
+			printd("Packet received from:\nPort:    "+rpkt.getPort()+"\nAddress: "+ rpkt.getAddress());
+			printd("Got the following bytes:\n"+byteToString(bRec)+"\n");
+			rec.byteParseFill(bRec);
+			rec.SetAddress(rpkt.getAddress());
+			rec.SetPort(rpkt.getPort());
+			return rec;
+		}
+		catch (Exception e) { e.printStackTrace(); System.exit(1); }
+		return rec;
+	}
+	
+	public Packet recievePacket(DatagramSocket soc, int timeout) throws IOException{
+		Packet rec = new Packet();
+		soc.setSoTimeout(timeout);
+		byte[] bRec = new byte[Packet.PACKETSIZE];
+		DatagramPacket rpkt = new DatagramPacket(bRec,bRec.length);
+		soc.receive(rpkt);
+		printd("Packet received from:\nPort:    "+rpkt.getPort()+"\nAddress: "+ rpkt.getAddress());
+		printd("Got the following bytes:\n"+byteToString(bRec)+"\n");
+		rec.byteParseFill(bRec);
+		rec.SetAddress(rpkt.getAddress());
+		rec.SetPort(rpkt.getPort());
+		
+		return rec;
+
+		
+		
+	}
+
+	
+	//Shortens the action of sending and receiving data;
+	public Packet sendReceive(Packet p, DatagramSocket soc, InetAddress addr, int port){
+		sendPacket(p, soc, addr, port);
+		printd("Awaiting response...");
+		Packet rec = recievePacket(soc);
+		printd("Bytes parsed into following Packet: "+rec+"\n");
+		return rec;
+	}
+
+	
+	//Allows the checking of a packet an whether or not its okay;
+	public boolean isOkay(Packet P, int Request){
+		if(P.GetRequest() == 5) {
+			print("Error received.");
+			return !handleError(P);
+		}
+		else if(P.GetRequest() != Request && Request!=0) {
+			print("Unexpected request.");
+			return false;
+		}
+		else if(!P.GetValid()) {
+			print("Invalid packet received.");
+			return false;
+		}
+		return true;
+	}
+	
+	//Allows Handles any error received;
+	public boolean handleError(Packet P){
+		if(P.GetRequest() == 5) {
+			print(P.toString()); 
+			return true;
+		}
+		return false;
+	}
+	
+	//For ease of sight, print data but avoid duplicating too many 0s without losing actual message
+	public String byteToString(byte[] arr){
+		String res = "";
+		int i = 0;
+		int j = 0;
+		for(i = 0; i < 4; i++){
+			if      (arr[i]<10 ) res+= "  ";
+			else if (arr[i]<100) res+=  " ";
+			res+=arr[i]+", ";
+			j++;
+			if(j==16){ j=0; res+="\n"; }
+		}
+		for(i = 4; i < arr.length; i++){
+			if(arr[i]==arr[i-1]&&arr[i]==0){}
+			else{
+				if      (arr[i]<10 ) res+= "  ";
+				else if (arr[i]<100) res+=  " ";
+				res+=arr[i]+", ";
+				j++;
+			}
+			if(j==16){ j=0; res+="\n"; }
+		}
+		return res;
+	}
+	
+	public String byteToStringW0(byte[] arr){
+		String res = "";
+		int i = 0;
+		int j = 0;
+		for(i = 0; i < arr.length; i++){
+			if      (arr[i]<10 ) res+= "  ";
+			else if (arr[i]<100) res+=  " ";
+			res+=arr[i]+", ";
+			j++;
+			if(j==16){ j=0; res+="\n"; }
+		}
+		return res;
+	}
+}
